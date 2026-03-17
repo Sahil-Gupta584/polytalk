6ac7c67fd280a42efe7bdf974d02e48a2afee8ae
Sahil Gupta <guptas3067@gmail.com>
2026-03-17 09:29:54 +0530

    build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -12,6 +12,10 @@ const config = defineConfig({
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
+    nitroV2Plugin({
+      preset: "vercel",
+      compatibilityDate: "2025-10-26",
+    }),
     tanstackStart(),
     viteReact(),
   ],
